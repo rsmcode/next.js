7a7b1e47ed956ba9900a50d9235719c2bc74e4fd
rsm <46247709+rsmcode@users.noreply.github.com>
2020-06-23 20:18:31 +0300

    redirect

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,23 +1,21 @@
-import Link from 'next/link'
-import React, { useEffect } from "react";
-import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-export default function IndexPage() {
-  const href = "auth0.samples.Auth0Sample://csat99.auth0.com/ios/auth0.samples.Auth0Sample/callback";
-  return (
-    <>
-      <Head>
-        {/* Redirects with meta refresh if no JavaScript support */}
-        <noscript>
-          <meta httpEquiv="refresh" content={`0;url=${href}`} />
-        </noscript>
-        <link rel="canonical" href={href} />
-
-      </Head>
-      {/* Provides a redirect link if no meta refresh support */}
-      <p>
-        Redirecting to <a href={href}>{href}</a>&hellip;
-        </p>
-    </>
-  )
+function RedirectPage({ ctx }) {
+  const router = useRouter()
+  // Make sure we're in the browser
+  if (typeof window !== 'undefined') {
+    router.push('auth0.samples.Auth0Sample://csat99.auth0.com/ios/auth0.samples.Auth0Sample/callback');
+    return;
+  }
 }
+
+RedirectPage.getInitialProps = ctx => {
+  // We check for ctx.res to make sure we're on the server.
+  if (ctx.res) {
+    ctx.res.writeHead(302, { Location: 'auth0.samples.Auth0Sample://csat99.auth0.com/ios/auth0.samples.Auth0Sample/callback' });
+    ctx.res.end();
+  }
+  return {};
+}
+
+export default RedirectPage
