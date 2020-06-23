c25246a6281b8d97d690b07a9fcae5cb15611292
rsm <46247709+rsmcode@users.noreply.github.com>
2020-06-23 19:35:57 +0300

    redirect

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -1,12 +1,23 @@
 import Link from 'next/link'
+import React, { useEffect } from "react";
+import Head from 'next/head'
 
 export default function IndexPage() {
+  const href = "https://www.google.com";
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        {/* Redirects with meta refresh if no JavaScript support */}
+        <noscript>
+          <meta httpEquiv="refresh" content={`0;url=${href}`} />
+        </noscript>
+        <link rel="canonical" href={href} />
+
+      </Head>
+      {/* Provides a redirect link if no meta refresh support */}
+      <p>
+        Redirecting to <a href={href}>{href}</a>&hellip;
+        </p>
+    </>
   )
 }
