269cad32577643bcbef04e681a83a772355175b7
rsm <46247709+rsmcode@users.noreply.github.com>
2020-06-23 22:47:17 +0300

    update

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -4,7 +4,7 @@ function RedirectPage({ ctx }) {
   const router = useRouter()
   // Make sure we're in the browser
   if (typeof window !== 'undefined') {
-    router.push('auth0.samples.Auth0Sample://csat99.auth0.com/ios/auth0.samples.Auth0Sample/callback');
+    router.push('auth0.samples.auth0sample://csat99.auth0.com/ios/auth0.samples.auth0sample/callback');
     return;
   }
 }
@@ -12,7 +12,7 @@ function RedirectPage({ ctx }) {
 RedirectPage.getInitialProps = ctx => {
   // We check for ctx.res to make sure we're on the server.
   if (ctx.res) {
-    ctx.res.writeHead(302, { Location: 'auth0.samples.Auth0Sample://csat99.auth0.com/ios/auth0.samples.Auth0Sample/callback' });
+    ctx.res.writeHead(302, { Location: 'auth0.samples.auth0sample://csat99.auth0.com/ios/auth0.samples.auth0sample/callback' });
     ctx.res.end();
   }
   return {};
