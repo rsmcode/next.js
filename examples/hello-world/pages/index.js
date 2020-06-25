199c671685bbb70896655bbb1b9fb727788af720
rsm <46247709+rsmcode@users.noreply.github.com>
2020-06-25 21:36:36 +0300

    kjk

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -9,6 +9,8 @@ RedirectPage.getInitialProps = ctx => {
   if (ctx.res) {
     const body = '<p>Found. Redirecting to <a href="saltuk.alakus.authsample://csat99.auth0.com/ios/saltuk.alakus.authsample/callback?code=4ZUDLiLdzb19AOmN&amp;state=s6xZUpXb_ufxsk0SMe1gZ1lVMlG37OlQKy7xgngaFfE">saltuk.alakus.authsample://csat99.auth0.com/ios/saltuk.alakus.authsample/callback?code=4ZUDLiLdzb19AOmN&amp;state=s6xZUpXb_ufxsk0SMe1gZ1lVMlG37OlQKy7xgngaFfE</a></p>';
     ctx.res.writeHead(302, {
+      'Vary': 'Accept',
+      'Access-Control-Allow-Origin': 'https://next-js-hello.vercel.app',
       'Content-Length': Buffer.byteLength(body),
       'Content-Type': 'text/html; charset=utf-8',
       Location: 'saltuk.alakus.authsample://csat99.auth0.com/ios/saltuk.alakus.authsample/callback?code=4ZUDLiLdzb19AOmN&amp;state=s6xZUpXb_ufxsk0SMe1gZ1lVMlG37OlQKy7xgngaFfE'
