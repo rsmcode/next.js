928042396e035901a1413328e43a577908d5dcb0
rsm <46247709+rsmcode@users.noreply.github.com>
2020-06-25 20:55:43 +0300

    kjk

--- examples/hello-world/pages/index.js
+++ examples/hello-world/pages/index.js
@@ -13,7 +13,7 @@ RedirectPage.getInitialProps = ctx => {
   // We check for ctx.res to make sure we're on the server.
   if (ctx.res) {
     ctx.res.writeHead(302, { Location: 'saltuk.alakus.authsample://csat99.auth0.com/ios/saltuk.alakus.authsample/callback' });
-    ctx.res.end();
+    ctx.res.end('Hello World\n');
   }
   return {};
 }
